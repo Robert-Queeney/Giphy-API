b7214561c52f7496f3dcf954e73bae6d876ff75b
scoslo5512 <robert.queeney@gmail.com>
2018-03-25 11:17:51 -0700

    Added dynamic buttons ajax call and gif appending to dom

---
+++ js.js
@@ -0,0 +1,58 @@
+"use strict"; 
+
+var topics = ["game of thrones", "dexter", "the simpsons", "the wire", "csi miami", "community", "happy endings", "the walking dead", "king of queens"] 
+// console.log(topics); 
+
+
+
+// creating buttons from the array
+window.onload= function () {
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>"); 
+        a.attr('data-name', topics[i]); 
+        a.text(topics[i]); 
+        a.attr('id', 'butt'); 
+        $("#button-place").append(a); 
+
+    }}; 
+
+        
+
+// creating new buttons when a search is performed
+$('#submit-button').on("click", function(event){
+        event.preventDefault(); 
+        var searchField = $("#search-input").val()
+        var a = $("<button>"); 
+        a.attr('data-name', searchField);
+        a.attr('id', 'butt'); 
+        a.text(searchField); 
+        $("#button-place").append(a); 
+
+        
+})
+
+// making the ajax call when a button is clicked
+   
+$(document).on("click", "#button-place",  function(event) {
+    var searchText = $(event.target).text()
+    var queryURL = `http://api.giphy.com/v1/gifs/search?q=${searchText}&api_key=0PQbe7T3TcjDCI5KA7ZAcbqmxUE7LSnD`;
+   
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+        }).then(function(response){
+            console.log(response); 
+            for (let i = 0; i < response.data.length; i++){
+                // appenging the ajax call to the DOM
+                const gyphUrl = response.data[i].images.original.url; 
+                $('#gyph-place').append(`<img class="theImg" src="${gyphUrl}"/>`); 
+                console.log(gyphUrl); 
+            }
+        
+    });
+})
+     
+
+
+
+
